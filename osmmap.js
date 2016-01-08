07acc2dd021fb8c96a5683640e6baf35169c014c
walley <walley@walley.org>
2016-01-08 15:58:50 +0100

    more map layers

--- osmmap.js
+++ osmmap.js
@@ -67,25 +67,69 @@ function load_data()
 
 function initmap() {
 // set up the map
-map = new L.Map('map');
+  map = new L.Map('map');
 
-// create the tile layer with correct attribution
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 19, attribution: osmAttrib});
+  var stamen = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
+    attribution: 'Map data CC-BY-SA <a href="http://openstreetmap.org">OSM.org</a>, imagery <a href="http://maps.stamen.com">Stamen Design</a>',
+    maxZoom: 18
+  });
 
-map.setView(new L.LatLng(49.5, 17), 7);
-map.addLayer(osm);
+  var osm = L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'OpenStreetMap.org'
+  });
 
-map.on('moveend', load_data);
-map.on('drag', function (e) { 
-  if (typeof xhr !== 'undefined') {
-    xhr.abort();
-  }
-});
-map.on('movestart', function (e) { 
-  if (typeof xhr !== 'undefined') {
-    xhr.abort();
-  }
-});
+  var mapbox = L.tileLayer('http://{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiemJ5Y3oiLCJhIjoiRUdkVEMzMCJ9.7eJ3YhCQtbVUET92En5aGA', {
+    attribution: 'OpenStreetMap.org & Mapbox'
+  });
+
+  var ocm = L.tileLayer("http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: 'Data: <a href="http://opencyclemap.org">OpenCycleMap</a>'
+  });
+
+  var hikebike = L.tileLayer("http://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: 'Data: <a href="http://www.hikebikemap.de">Hike &amp; Bike Map</a>'
+  });
+
+  var mtb = L.tileLayer("http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: 'OpenStreetMap.org a USGS'
+  });
+
+  var baseLayers = {
+    "Mapbox streets": mapbox.addTo(map),
+    "MTBMap.cz": mtb,
+    "OpenStreetMap Mapnik": osm,
+    "OpenCycleMap": ocm,
+    "Hike&bike": hikebike,
+    "Vodovky": stamen
+  };
+
+
+  // create the tile layer with correct attribution
+  var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osm2 = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 19, attribution: osmAttrib});
+
+  map.setView(new L.LatLng(49.5, 17), 7);
+  map.addLayer(osm2);
+
+  var overlays = {};
+
+  L.control.layers(baseLayers, overlays).addTo(map);
+  L.control.scale().addTo(map);
+
+  map.on('moveend', load_data);
+  map.on('drag', function (e) { 
+    if (typeof xhr !== 'undefined') {
+      xhr.abort();
+    }
+  });
+  map.on('movestart', function (e) { 
+    if (typeof xhr !== 'undefined') {
+      xhr.abort();
+    }
+  });
 }
