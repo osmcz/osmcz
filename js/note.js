a7d8b19fca1ad934807bb249ce334d614664088b
U-ol-something\Tereza <Tereza@ol-vmwmgmt.ol.hasici-ol.cz>
2016-01-13 13:07:41 +0100

    class note for osm note handling , new file

---
+++ js/note.js
@@ -0,0 +1,26 @@
+function note(lat, lon, text) {
+
+    this.lat = lat;
+    this.lon = lon;
+    this.note = note;
+
+    this.send = function() {
+        var jqxhr = $.ajax({
+                url: "http://map.openstreetmap.cz/upload.php",
+                type: "post", //send it through get method
+                data: {
+                    lat: this.lat,
+                    lon: this.lon,
+                    text: "nazdar"
+                },
+            })
+            .done(function(data) {
+                alert("success " + data);
+            })
+            .fail(function() {
+                alert("error");
+            })
+            .always(function() {});
+    }
+
+}
