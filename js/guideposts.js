8fa2a22adc60560a8c815b489d92a24dccac16e3
walley <walley@walley.org>
2016-01-18 08:33:23 +0100

    readd header, dual licensed

--- js/guideposts.js
+++ js/guideposts.js
@@ -1,4 +1,26 @@
-// (c) 2016 osmcz-app, https://github.com/osmcz/osmcz
+/*
+ guideposts for osmcz
+ Javascript code for openstreetmap.cz website
+ Copyright (C) 2015,2016
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+and
+
+ (c) 2016 osmcz-app, https://github.com/osmcz/osmcz
+
+ */
 
 var osmcz = osmcz || {};
 osmcz.guideposts = function(map, baseLayers, overlays, controls) {
@@ -45,6 +67,7 @@ osmcz.guideposts = function(map, baseLayers, overlays, controls) {
     map.on('layeradd', function(event) {
         if(event.layer == markers) {
             load_data()
+            alert('x');
         }
     });
 
