9919217ebc1401a177577451aec00266e44e473d
zbycz <zbytovsky@gmail.com>
2016-01-29 16:04:51 +0100

    release 0.6

--- js/osmcz.js
+++ js/osmcz.js
@@ -1,4 +1,4 @@
-var OSMCZ_APP_VERSION = '0.5';
+var OSMCZ_APP_VERSION = '0.6';
 
 var osmcz = osmcz || {};
 osmcz.basePath = ['openstreetmap.cz', 'osmap.cz', 'osm.localhost'].indexOf(location.hostname) !== -1 ? '/theme/' : '';
