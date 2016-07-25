31fa544198fff963b0003c3696940c707a42ffda
walley <walley@walley.org>
2016-07-25 09:13:49 +0200

    note: notification through api added

--- js/note.js
+++ js/note.js
@@ -1,17 +1,45 @@
-function note(lat, lon, text) {
+/*
+ guideposts for osmcz
+ Javascript code for openstreetmap.cz website
+ Copyright (C) 2015,2016
 
-    this.lat = lat;
-    this.lon = lon;
-    this.note = note;
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    this.send = function() {
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+and
+
+ (c) 2016 osmcz-app, https://github.com/osmcz/osmcz
+
+*/
+
+
+/*
+  This is platform for various notifications, notes, and other ways os sending stuff.
+*/
+
+var osmcz = osmcz || {};
+
+osmcz.note = function() {
+
+    osmcz.note.prototype.note_api = function(lat, lon, text)
+    {
         var jqxhr = $.ajax({
                 url: "https://api.openstreetmap.cz/table/notify",
                 type: "post",
                 data: {
-                    lat: this.lat,
-                    lon: this.lon,
-                    text: this.text
+                    lat: lat,
+                    lon: lon,
+                    text: text
                 },
             })
             .done(function(data) {
@@ -20,6 +48,11 @@ function note(lat, lon, text) {
                 alert("error");
             })
             .always(function() {});
+    }
+
+    osmcz.note.prototype.note_osm = function(lat, lon, text)
+    {
+      //osm notes nothing here yet
     }
 
 }
