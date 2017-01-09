a38bcb65cd5e2ad3827fbc3e7b1e898bcd6694e2
Pavel Zbytovský <zbytovsky@gmail.com>
2017-01-09 09:46:24 +0100

    release 0.13

--- js/osmcz.js
+++ js/osmcz.js
@@ -1,4 +1,4 @@
-var OSMCZ_APP_VERSION = '0.12';
+var OSMCZ_APP_VERSION = '0.13';
 
 var osmcz = osmcz || {};
 osmcz.basePath = ['openstreetmap.cz', 'osmap.cz', 'osm.localhost'].indexOf(location.hostname) !== -1 ? '/theme/' : '';
