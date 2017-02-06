fdfa0011a83a213128b9727a0bd2170861b7b831
Marián Kyral <mkyral@email.cz>
2017-02-06 19:50:19 +0100

    js/layer-switcher.js: formating: tabs -> spaces

--- js/layer-switcher.js
+++ js/layer-switcher.js
@@ -5,261 +5,261 @@
  */
 
 osmcz.LayerSwitcher = L.Control.extend({
-	options: {
-		collapsed: true,
-		position: 'topright',
-		autoZIndex: true
-	},
+    options: {
+        collapsed: true,
+        position: 'topright',
+        autoZIndex: true
+    },
 
-	initialize: function (baseLayers, overlays, options) {
-		L.setOptions(this, options);
+    initialize: function (baseLayers, overlays, options) {
+        L.setOptions(this, options);
 
-		this._layers = {};
-		this._lastZIndex = 0;
-		this._handlingClick = false;
+        this._layers = {};
+        this._lastZIndex = 0;
+        this._handlingClick = false;
 
-		for (var i in baseLayers) {
-			this._addLayer(baseLayers[i], i);
-		}
+        for (var i in baseLayers) {
+            this._addLayer(baseLayers[i], i);
+        }
 
-		for (i in overlays) {
-			this._addLayer(overlays[i], i, true);
-		}
-	},
+        for (i in overlays) {
+            this._addLayer(overlays[i], i, true);
+        }
+    },
 
-	onAdd: function (map) {
-		this._initLayout();
-		this._update();
+    onAdd: function (map) {
+        this._initLayout();
+        this._update();
 
-		map
-		    .on('layeradd', this._onLayerChange, this)
-		    .on('layerremove', this._onLayerChange, this);
+        map
+            .on('layeradd', this._onLayerChange, this)
+            .on('layerremove', this._onLayerChange, this);
 
-		return this._container;
-	},
+        return this._container;
+    },
 
-	onRemove: function (map) {
-		map
-		    .off('layeradd', this._onLayerChange, this)
-		    .off('layerremove', this._onLayerChange, this);
-	},
+    onRemove: function (map) {
+        map
+            .off('layeradd', this._onLayerChange, this)
+            .off('layerremove', this._onLayerChange, this);
+    },
 
-	addBaseLayer: function (layer, name) {
-		this._addLayer(layer, name);
-		this._update();
-		return this;
-	},
+    addBaseLayer: function (layer, name) {
+        this._addLayer(layer, name);
+        this._update();
+        return this;
+    },
 
-	addOverlay: function (layer, name) {
-		this._addLayer(layer, name, true);
-		this._update();
-		return this;
-	},
+    addOverlay: function (layer, name) {
+        this._addLayer(layer, name, true);
+        this._update();
+        return this;
+    },
 
-	removeLayer: function (layer) {
-		var id = L.stamp(layer);
-		delete this._layers[id];
-		this._update();
-		return this;
-	},
+    removeLayer: function (layer) {
+        var id = L.stamp(layer);
+        delete this._layers[id];
+        this._update();
+        return this;
+    },
 
-	_initLayout: function () {
-		var className = 'leaflet-control-layers',
-		    container = this._container = L.DomUtil.create('div', className);
+    _initLayout: function () {
+        var className = 'leaflet-control-layers',
+            container = this._container = L.DomUtil.create('div', className);
 
-		//Makes this work on IE10 Touch devices by stopping it from firing a mouseout event when the touch is released
-		container.setAttribute('aria-haspopup', true);
+        //Makes this work on IE10 Touch devices by stopping it from firing a mouseout event when the touch is released
+        container.setAttribute('aria-haspopup', true);
 
-		if (!L.Browser.touch) {
-			L.DomEvent
-				.disableClickPropagation(container)
-				.disableScrollPropagation(container);
-		} else {
-			L.DomEvent.on(container, 'click', L.DomEvent.stopPropagation);
-		}
+        if (!L.Browser.touch) {
+            L.DomEvent
+                .disableClickPropagation(container)
+                .disableScrollPropagation(container);
+        } else {
+            L.DomEvent.on(container, 'click', L.DomEvent.stopPropagation);
+        }
 
-		var form = this._form = L.DomUtil.create('form', className + '-list');
+        var form = this._form = L.DomUtil.create('form', className + '-list');
 
-    $('#map-layers button.close').click(function(){
-      $('#map-layers').hide();
-      $(container).show();
-    });
+        $('#map-layers button.close').click(function(){
+            $('#map-layers').hide();
+            $(container).show();
+        });
 
 
-		if (this.options.collapsed) {
-			if (!L.Browser.android) {
-				L.DomEvent
+        if (this.options.collapsed) {
+            if (!L.Browser.android) {
+                L.DomEvent
             .on(container, 'click', this._expand, this)  //osmcz
             //.on(container, 'mouseover', this._expand, this)
-				    .on(container, 'mouseout', this._collapse, this);
-			}
-			var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
-			link.href = '#';
-			link.title = 'Layers';
+                    .on(container, 'mouseout', this._collapse, this);
+            }
+            var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
+            link.href = '#';
+            link.title = 'Layers';
 
-			if (L.Browser.touch) {
-				L.DomEvent
-				    .on(link, 'click', L.DomEvent.stop)
-				    .on(link, 'click', this._expand, this);
-			}
-			else {
-				L.DomEvent.on(link, 'focus', this._expand, this);
-			}
-			//Work around for Firefox android issue https://github.com/Leaflet/Leaflet/issues/2033
-			L.DomEvent.on(form, 'click', function () {
-				setTimeout(L.bind(this._onInputClick, this), 0);
-			}, this);
+            if (L.Browser.touch) {
+                L.DomEvent
+                    .on(link, 'click', L.DomEvent.stop)
+                    .on(link, 'click', this._expand, this);
+            }
+            else {
+                L.DomEvent.on(link, 'focus', this._expand, this);
+            }
+            //Work around for Firefox android issue https://github.com/Leaflet/Leaflet/issues/2033
+            L.DomEvent.on(form, 'click', function () {
+                setTimeout(L.bind(this._onInputClick, this), 0);
+            }, this);
 
-			this._map.on('click', this._collapse, this);
-			// TODO keyboard accessibility
-		} else {
-			this._expand();
-		}
+            this._map.on('click', this._collapse, this);
+            // TODO keyboard accessibility
+        } else {
+            this._expand();
+        }
 
-		this._baseLayersList = L.DomUtil.create('div', className + '-base', form);
-		this._separator = L.DomUtil.create('div', className + '-separator', form);
-		this._overlaysList = L.DomUtil.create('div', className + '-overlays', form);
+        this._baseLayersList = L.DomUtil.create('div', className + '-base', form);
+        this._separator = L.DomUtil.create('div', className + '-separator', form);
+        this._overlaysList = L.DomUtil.create('div', className + '-overlays', form);
 
     //container.appendChild(form);
     $('#map-layers-content').append(form);
-	},
+    },
 
-	_addLayer: function (layer, name, overlay) {
-		var id = L.stamp(layer);
+    _addLayer: function (layer, name, overlay) {
+        var id = L.stamp(layer);
 
-		this._layers[id] = {
-			layer: layer,
-			name: name,
-			overlay: overlay
-		};
+        this._layers[id] = {
+            layer: layer,
+            name: name,
+            overlay: overlay
+        };
 
-		if (this.options.autoZIndex && layer.setZIndex) {
-			this._lastZIndex++;
-			layer.setZIndex(this._lastZIndex);
-		}
-	},
+        if (this.options.autoZIndex && layer.setZIndex) {
+            this._lastZIndex++;
+            layer.setZIndex(this._lastZIndex);
+        }
+    },
 
-	_update: function () {
-		if (!this._container) {
-			return;
-		}
+    _update: function () {
+        if (!this._container) {
+            return;
+        }
 
-		this._baseLayersList.innerHTML = '';
-		this._overlaysList.innerHTML = '';
+        this._baseLayersList.innerHTML = '';
+        this._overlaysList.innerHTML = '';
 
-		var baseLayersPresent = false,
-		    overlaysPresent = false,
-		    i, obj;
+        var baseLayersPresent = false,
+            overlaysPresent = false,
+            i, obj;
 
-		for (i in this._layers) {
-			obj = this._layers[i];
-			this._addItem(obj);
-			overlaysPresent = overlaysPresent || obj.overlay;
-			baseLayersPresent = baseLayersPresent || !obj.overlay;
-		}
+        for (i in this._layers) {
+            obj = this._layers[i];
+            this._addItem(obj);
+            overlaysPresent = overlaysPresent || obj.overlay;
+            baseLayersPresent = baseLayersPresent || !obj.overlay;
+        }
 
-		this._separator.style.display = overlaysPresent && baseLayersPresent ? '' : 'none';
-	},
+        this._separator.style.display = overlaysPresent && baseLayersPresent ? '' : 'none';
+    },
 
-	_onLayerChange: function (e) {
-		var obj = this._layers[L.stamp(e.layer)];
+    _onLayerChange: function (e) {
+        var obj = this._layers[L.stamp(e.layer)];
 
-		if (!obj) { return; }
+        if (!obj) { return; }
 
-		if (!this._handlingClick) {
-			this._update();
-		}
+        if (!this._handlingClick) {
+            this._update();
+        }
 
-		var type = obj.overlay ?
-			(e.type === 'layeradd' ? 'overlayadd' : 'overlayremove') :
-			(e.type === 'layeradd' ? 'baselayerchange' : null);
+        var type = obj.overlay ?
+            (e.type === 'layeradd' ? 'overlayadd' : 'overlayremove') :
+            (e.type === 'layeradd' ? 'baselayerchange' : null);
 
-		if (type) {
-			this._map.fire(type, obj);
-		}
-	},
+        if (type) {
+            this._map.fire(type, obj);
+        }
+    },
 
-	// IE7 bugs out if you create a radio dynamically, so you have to do it this hacky way (see http://bit.ly/PqYLBe)
-	_createRadioElement: function (name, checked) {
+    // IE7 bugs out if you create a radio dynamically, so you have to do it this hacky way (see http://bit.ly/PqYLBe)
+    _createRadioElement: function (name, checked) {
 
-		var radioHtml = '<input type="radio" class="leaflet-control-layers-selector" name="' + name + '"';
-		if (checked) {
-			radioHtml += ' checked="checked"';
-		}
-		radioHtml += '/>';
+        var radioHtml = '<input type="radio" class="leaflet-control-layers-selector" name="' + name + '"';
+        if (checked) {
+            radioHtml += ' checked="checked"';
+        }
+        radioHtml += '/>';
 
-		var radioFragment = document.createElement('div');
-		radioFragment.innerHTML = radioHtml;
+        var radioFragment = document.createElement('div');
+        radioFragment.innerHTML = radioHtml;
 
-		return radioFragment.firstChild;
-	},
+        return radioFragment.firstChild;
+    },
 
-	_addItem: function (obj) {
-		var label = document.createElement('label'),
-		    input,
-		    checked = this._map.hasLayer(obj.layer);
+    _addItem: function (obj) {
+        var label = document.createElement('label'),
+            input,
+            checked = this._map.hasLayer(obj.layer);
 
-		if (obj.overlay) {
-			input = document.createElement('input');
-			input.type = 'checkbox';
-			input.className = 'leaflet-control-layers-selector';
-			input.defaultChecked = checked;
-		} else {
-			input = this._createRadioElement('leaflet-base-layers', checked);
-		}
+        if (obj.overlay) {
+            input = document.createElement('input');
+            input.type = 'checkbox';
+            input.className = 'leaflet-control-layers-selector';
+            input.defaultChecked = checked;
+        } else {
+            input = this._createRadioElement('leaflet-base-layers', checked);
+        }
 
-		input.layerId = L.stamp(obj.layer);
+        input.layerId = L.stamp(obj.layer);
 
-		L.DomEvent.on(input, 'click', this._onInputClick, this);
+        L.DomEvent.on(input, 'click', this._onInputClick, this);
 
-		var name = document.createElement('span');
-		name.innerHTML = ' ' + obj.name;
+        var name = document.createElement('span');
+        name.innerHTML = ' ' + obj.name;
 
-		label.appendChild(input);
-		label.appendChild(name);
+        label.appendChild(input);
+        label.appendChild(name);
 
-		var container = obj.overlay ? this._overlaysList : this._baseLayersList;
-		container.appendChild(label);
+        var container = obj.overlay ? this._overlaysList : this._baseLayersList;
+        container.appendChild(label);
 
-		return label;
-	},
+        return label;
+    },
 
-	_onInputClick: function () {
-		var i, input, obj,
-		    inputs = this._form.getElementsByTagName('input'),
-		    inputsLen = inputs.length;
+    _onInputClick: function () {
+        var i, input, obj,
+            inputs = this._form.getElementsByTagName('input'),
+            inputsLen = inputs.length;
 
-		this._handlingClick = true;
+        this._handlingClick = true;
 
-		for (i = 0; i < inputsLen; i++) {
-			input = inputs[i];
-			obj = this._layers[input.layerId];
+        for (i = 0; i < inputsLen; i++) {
+            input = inputs[i];
+            obj = this._layers[input.layerId];
 
-			if (input.checked && !this._map.hasLayer(obj.layer)) {
-				this._map.addLayer(obj.layer);
+            if (input.checked && !this._map.hasLayer(obj.layer)) {
+                this._map.addLayer(obj.layer);
 
-			} else if (!input.checked && this._map.hasLayer(obj.layer)) {
-				this._map.removeLayer(obj.layer);
-			}
-		}
+            } else if (!input.checked && this._map.hasLayer(obj.layer)) {
+                this._map.removeLayer(obj.layer);
+            }
+        }
 
-		this._handlingClick = false;
+        this._handlingClick = false;
 
-		this._refocusOnMap();
-	},
+        this._refocusOnMap();
+    },
 
-	_expand: function () {
+    _expand: function () {
     $('#map-layers').show();
     $(this._container).hide();
 
-		//L.DomUtil.addClass(this._container, 'leaflet-control-layers-expanded');
-	},
+        //L.DomUtil.addClass(this._container, 'leaflet-control-layers-expanded');
+    },
 
-	_collapse: function () {
-		//this._container.className = this._container.className.replace(' leaflet-control-layers-expanded', '');
-	}
+    _collapse: function () {
+        //this._container.className = this._container.className.replace(' leaflet-control-layers-expanded', '');
+    }
 });
 
 osmcz.layerSwitcher = function (baseLayers, overlays, options) {
-	return new osmcz.LayerSwitcher(baseLayers, overlays, options);
+    return new osmcz.LayerSwitcher(baseLayers, overlays, options);
 };
