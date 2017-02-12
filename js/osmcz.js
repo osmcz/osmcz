5fa7ce95a811da7456bbc7e31b9a068969b96c5c
Marián Kyral <mkyral@email.cz>
2017-02-12 19:02:09 +0100

    release 0.15

--- js/osmcz.js
+++ js/osmcz.js
@@ -1,4 +1,4 @@
-var OSMCZ_APP_VERSION = '0.14';
+var OSMCZ_APP_VERSION = '0.15';
 
 var osmcz = osmcz || {};
 osmcz.production = ['openstreetmap.cz', 'osmap.cz', 'osm.localhost', 'zby.cz'].indexOf(location.hostname) !== -1;
