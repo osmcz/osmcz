5c5c00b7c43d06d77da1efe6fe16bb09b5f399e6
Pavel Zbytovský <zbytovsky@gmail.com>
2017-04-02 11:09:30 +0200

    layers: added openiframap; refactoring, maxZooms, https

--- js/layers.js
+++ js/layers.js
@@ -9,9 +9,11 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
     var osmAttr = '&copy; <a href="https://openstreetmap.org/copyright">OSM</a>'; //abbrevation not recommended on other websites
 
     var thunderforestAPIkey = '00291b657a5d4c91bbacb0ff096e2c25';
+    var mapboxAPIkey = "pk.eyJ1IjoiemJ5Y3oiLCJhIjoiY2owa3hrYjF3MDAwejMzbGM4aDNybnhtdyJ9.8CIw6X6Jvmk2GwCE8Zx8SA";
 
 
-    var mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}' + retinaSuffix + '.png?access_token=pk.eyJ1IjoiemJ5Y3oiLCJhIjoiY2owa3hrYjF3MDAwejMzbGM4aDNybnhtdyJ9.8CIw6X6Jvmk2GwCE8Zx8SA', {
+    var mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}' + retinaSuffix + '.png?access_token=' + mapboxAPIkey, {
+        maxZoom: 24,
         attribution: osmAttr + ", <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>",
         code: 'x',
         osmczDefaultLayer: true,
@@ -20,12 +22,11 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
 
     var turisticka = L.tileLayer("https://tile.poloha.net/{z}/{x}/{y}.png", {
         maxZoom: 20,
-        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>', // @TODO: upravit, až bude funkční HTTPS verze
-        code: 'k',
-        basic: true
+        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>',
+        code: 'k'
     });
 
-    var opentopomap = L.tileLayer("https://{s}.tile.opentopomap.org//{z}/{x}/{y}.png", {
+    var opentopomap = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
         maxZoom: 15,
         attribution: osmAttr + ', <a href="https://opentopomap.org/">OpenTopoMap</a>',
         code: 'u'
@@ -38,29 +39,37 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         basic: true
     });
 
-    var wikimediamap = L.tileLayer(" https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}" + retinaSuffix + ".png", {
+    var wikimediamap = L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}" + retinaSuffix + ".png", {
         maxZoom: 18,
-        attribution: osmAttr + ', <a href="https://www.mediawiki.org/wiki/Maps/">Wikimedia</a>',
+        attribution: osmAttr + ', <a href="https://www.mediawiki.org/wiki/Maps">Wikimedia</a>',
         code: 'w'
     });
 
     var ocm = L.tileLayer("https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}" + retinaSuffix + ".png?apikey=" + thunderforestAPIkey, {
-        maxZoom: 18,
+        maxZoom: 22,
         attribution: osmAttr + ', <a href="https://opencyclemap.org">OpenCycleMap</a>',
         code: 'c',
         basic: true
     });
 
+    var thunderoutdoor = L.tileLayer("https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}" + retinaSuffix + ".png?apikey=" + thunderforestAPIkey, {
+        maxZoom: 22,
+        attribution: osmAttr + ', <a href="https://www.thunderforest.com/maps/outdoors/">Thunderforest</a>',
+        code: 'f'
+    });
+
+
     var hikebike = L.tileLayer(osmcz.fakeHttps + "{s}.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png", {
         maxZoom: 18,
-        attribution: osmAttr + ', <a href="http://www.hikebikemap.org">Hike &amp; Bike Map</a>', // @TODO: upravit, až bude HTTPS verze
+        attribution: osmAttr + ', <a href="http://www.hikebikemap.org">Hike &amp; Bike Map</a>',
         code: 'h'
     });
 
     var mtb = L.tileLayer(osmcz.fakeHttps + "tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png", {
         maxZoom: 18,
-        attribution: osmAttr + ', <a href="http://www.mtbmap.cz">mtbmap.cz</a>', // @TODO: upravit, až bude HTTPS verze
-        code: 'm'
+        attribution: osmAttr + ', <a href="http://www.mtbmap.cz">mtbmap.cz</a>',
+        code: 'm',
+        basic: true
     });
 
     var vodovky = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
@@ -76,7 +85,7 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
     });
 
     var dopravni = L.tileLayer("https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}" + retinaSuffix + ".png?apikey=" + thunderforestAPIkey, {
-        maxZoom: 18,
+        maxZoom: 22,
         attribution: osmAttr + ', <a href="https://www.thunderforest.com/maps/transport/">Thunderforest</a>',
         code: 't',
         basic: true
@@ -88,9 +97,9 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         code: 'ö'
     });
 
-    var menepopisku = L.tileLayer(osmcz.fakeHttps + "{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}" + retinaSuffix + ".png", {
-        maxZoom: 18,
-        attribution: osmAttr + ', <a href="https://cartodb.com/attributions#basemaps">CartoDB</a>',
+    var menepopisku = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}" + retinaSuffix + ".png", {
+        maxZoom: 24, //umi az 30 a mozna i vic
+        attribution: osmAttr + ', <a href="https://carto.com/attributions">CARTO</a>',
         code: 'b'
     });
 
@@ -101,34 +110,34 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         crs: L.CRS.EPSG4326,
         minZoom: 7,
         maxZoom: 22,
-        attribution: '&copy; <a href="http://www.cuzk.cz">ČÚZK</a>', // @TODO: upravit, až bude HTTPS verze
+        attribution: '&copy; <a href="http://www.cuzk.cz">ČÚZK</a>',
         code: 'o',
         basic: true
     });
 
     var metropolis = L.tileLayer("https://api.mapbox.com/styles/v1/severak/cinr478gg00aucam0o6lran4v/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2V2ZXJhayIsImEiOiJjaXQxenM2ZTEwMGIyMnRwZGMwZzF6Y2VsIn0.-uZbcCAI3ABqnbg6h1mrhQ", {
-        maxZoom: 18,
+        maxZoom: 24,
         attribution: osmAttr + ', <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a>, <a href=\'http://severak.svita.cz\'>Severák</a>',
         code: 'r'
     });
 
     var spinal = L.tileLayer("https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}" + retinaSuffix + ".png?apikey=" + thunderforestAPIkey, {
-        maxZoom: 18,
+        maxZoom: 22,
         attribution: osmAttr + ', <a href="https://www.thunderforest.com/maps/spinal-map/">Thunderforest</a>',
         code: 'a'
     });
 
     var pioneer = L.tileLayer("https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}" + retinaSuffix + ".png?apikey=" + thunderforestAPIkey, {
-        maxZoom: 18,
+        maxZoom: 22,
         attribution: osmAttr + ', <a href="https://www.thunderforest.com/maps/pioneer/">Thunderforest</a>',
         code: 'p'
     });
 
+    // automatically add/remove orotofotoOverlay
     map.on('baselayerchange', function (event) {
         if (event.layer == ortofoto || event.layer == vodovky) {
             setTimeout(function(){  // needs timeout or doesnt work
                 if (!map.hasLayer(ortofotoOverlay)) {
-//                     console.log('added overlay');
                     map.addLayer(ortofotoOverlay);
                 }
 
@@ -144,7 +153,6 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
             }, 300);
         } else {
               setTimeout(function(){  // needs timeout or doesnt work
-//                     console.log('removed overlay');
                     if (map.hasLayer(ortofotoOverlay)) {
                       map.removeLayer(ortofotoOverlay);
                     }
@@ -162,14 +170,14 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
 
     var turistikaOverlay = L.tileLayer("https://tile.poloha.net/kct/{z}/{x}/{y}.png", {
         maxZoom: 20,
-        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>', // @TODO: upravit, až bude HTTPS verze
+        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>',
         opacity: 0.6,
         code: 'K',
         basic: true
     });
 
-    var ortofotoOverlay = L.tileLayer("https://{s}.tiles.mapbox.com/v4/zbycz.e9b65202/{z}/{x}/{y}" + retinaSuffix + ".png?access_token=pk.eyJ1IjoiemJ5Y3oiLCJhIjoiY2owa3hrYjF3MDAwejMzbGM4aDNybnhtdyJ9.8CIw6X6Jvmk2GwCE8Zx8SA", {
-        maxZoom: 22,
+    var ortofotoOverlay = L.tileLayer("https://{s}.tiles.mapbox.com/v4/zbycz.e9b65202/{z}/{x}/{y}" + retinaSuffix + ".png?access_token=" + mapboxAPIkey, {
+        maxZoom: 24,
         attribution: osmAttr + ", <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>",
         opacity: 1,
         code: 'O'
@@ -179,16 +187,11 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
     var vrstevniceOverlayOrtoUrl = "https://tile.poloha.net/contours_ortofoto/{z}/{x}/{y}.png";
     var vrstevniceOverlay = L.tileLayer(vrstevniceOverlayUrl, {
         maxZoom: 20,
-        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>', // @TODO: upravit, až bude HTTPS verze
+        attribution: osmAttr + ', <a href="http://www.poloha.net">poloha.net</a>',
         opacity: 0.6,
         code: 'V'
     });
 
-//     var zimniOverlay = L.tileLayer(osmcz.fakeHttps + "www.opensnowmap.org/opensnowmap-overlay/{z}/{x}/{y}.png", {
-//         maxZoom: 18,
-//         attribution: osmAttr + ', <a href="http://www.opensnowmap.org">opensnowmap.org</a>', // @TODO: upravit, až bude HTTPS verze
-//         code: 'Z'
-//     });
     var zimniOverlay = L.tileLayer("https://www.opensnowmap.org/tiles-pistes/{z}/{x}/{y}.png", {
         maxZoom: 18,
         attribution: osmAttr + ', <a href="http://www.opensnowmap.org">opensnowmap.org</a>',
@@ -211,17 +214,16 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         basic: true
     });
 
-    var katastralniMapaOverlayLayers = 'parcelni_cisla,obrazy_parcel,RST_KMD,hranice_parcel,DEF_BUDOVY,RST_KN,dalsi_p_mapy,prehledka_kat_prac,prehledka_kat_uz,prehledka_kraju-linie'
+    var katastralniMapaOverlayLayers = 'parcelni_cisla,obrazy_parcel,RST_KMD,hranice_parcel,DEF_BUDOVY,RST_KN,dalsi_p_mapy,prehledka_kat_prac,prehledka_kat_uz,prehledka_kraju-linie',
         katastralniMapaOverlayLayersOrto = 'parcelni_cisla_i,obrazy_parcel_i,RST_KMD_I,hranice_parcel_i,DEF_BUDOVY,RST_KN_I,dalsi_p_mapy_i,prehledka_kat_prac,prehledka_kat_uz,prehledka_kraju-linie';
     var katastralniMapaOverlay = L.tileLayer.wms('https://services.cuzk.cz/wms/wms.asp', {
-//         layers: 'DEF_PARCELY,DEF_BUDOVY,RST_PK_I,RST_KMD_I,dalsi_p_mapy_i,obrazy_parcel_i,parcelni_cisla_i,hranice_parcel_barevne,omp_i',
         layers: katastralniMapaOverlayLayers,
         format: 'image/png',
         transparent: true,
         crs: L.CRS.EPSG3857,
         minZoom: 7,
         maxZoom: 22,
-        attribution: '&copy; <a href="http://www.cuzk.cz">ČÚZK</a>', // @TODO: upravit, až bude HTTPS verze
+        attribution: '&copy; <a href="http://www.cuzk.cz">ČÚZK</a>',
         code: 'X'
     });
 
@@ -230,7 +232,7 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         format: 'image/png',
         transparent: true,
         crs: L.CRS.EPSG4326,
-        attribution: " <a href='http://www.eagri.cz.cz'>eagri.cz</a>", // @TODO: upravit, až bude HTTPS verze
+        attribution: " <a href='http://www.eagri.cz.cz'>eagri.cz</a>",
         code: 'L'
     });
 
@@ -251,7 +253,16 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
         ruianLanduseOverlay = L.tileLayer(landuseUrl, {minZoom: 12, maxZoom: 20, attribution: ruianAttr, code: '5'}),
         ruianAdresyOverlay = L.tileLayer(adresyUrl, {minZoom: 12, maxZoom: 20, attribution: ruianAttr, code: '6'});
 
-    var aktivniOverlay = new osmcz.activeLayer(map);
+    var aktivniVrstvaOverlay = new osmcz.activeLayer(map);
+
+    var powerOverlay = L.tileLayer('https://tiles-{s}.openinframap.org/power/{z}/{x}/{y}.png', {
+        attribution: osmAttr + ', <a href="https://OpenInfraMap.org/about.html">OpenInfraMap.org</a>',
+        code: 'W'
+    });
+    var commsOverlay = L.tileLayer('https://tiles-{s}.openinframap.org/telecoms/{z}/{x}/{y}.png', {
+        attribution: osmAttr + ', <a href="https://OpenInfraMap.org/about.html">OpenInfraMap.org</a>',
+        code: 'M'
+    });
 
 
     // Base group
@@ -273,12 +284,12 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
     // Information group
     baseLayers["Informace"] = {};
     overlays["Informace"] = {};
-    overlays["Informace"]["Aktivní vrstva"] = aktivniOverlay;
+    overlays["Informace"]["Aktivní vrstva"] = aktivniVrstvaOverlay;
 
     // Hiking group
     baseLayers["Turistické"] = {};
-    baseLayers["Turistické"]["Turistická mapa"] = turisticka;
-    baseLayers["Turistické"]["MTBMap.cz"] = mtb;
+    baseLayers["Turistické"]["Turistická mapa (ČR)"] = turisticka;
+    baseLayers["Turistické"]["Cyklo+turistická (EU)"] = mtb;
     baseLayers["Turistické"]["Hikebikemap.org"] = hikebike;
 
     overlays["Turistické"] = {};
@@ -309,6 +320,8 @@ osmcz.layers = function (map, baseLayers, overlays, controls) {
     // Special group
     overlays["Speciální"] = {};
     overlays["Speciální"]["Katastrální mapa ČÚZK"] = katastralniMapaOverlay;
+    overlays["Speciální"]["Vedení vysokého napětí"] = powerOverlay;
+    overlays["Speciální"]["Telekomunikační vysílače"] = commsOverlay;
     overlays["Speciální"]["Pole a louky (pLPIS)"] = lpisOverlay;
 
     // RUIAN group
