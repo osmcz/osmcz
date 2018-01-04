45e1bbcd9238a832a64c76798e2bf8cb7e7afc5e
Pavel Zbytovský <zbytovsky@gmail.com>
2018-01-04 23:29:49 +0100

    added changes from "talkcz browser" latte+css+ajax 2017-01-04 osmcz/website@4ab986659823cfd4ade369e5d96f520212d5a30e

--- js/jquery.nette.custom.js
+++ js/jquery.nette.custom.js
@@ -37,19 +37,42 @@ jQuery.extend({
                     jQuery.nette.updateSnippet(i, payload.snippets[i]);
                 }
             }
+
+            // history api
+            if (payload.uri) {
+                history.pushState({nette: true}, document.title, "/" + payload.uri);
+                window.scrollTo(0,0);
+                if (this.url && this.url.match(/#(.+)$/)) {
+                    var id = /#(.+)$/.exec(this.url)[0];
+                    $('html, body').animate({scrollTop: $(id).offset().top}, 200);
+                }
+            }
+        },
+
+        callUrl: function (obj) {
+            var src = 'data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH+GkNyZWF0ZWQgd2l0aCBhamF4bG9hZC5pbmZvACH5BAAKAAAAIf8LTkVUU0NBUEUyLjADAQAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQACgABACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkEAAoAAgAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkEAAoAAwAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkEAAoABAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQACgAFACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQACgAGACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAAKAAcALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==';
+            var img = document.createElement('img');
+            img.src = src;
+            img.style.position = 'absolute';
+            img.style.paddingTop = '3px';
+            obj.parentNode.insertBefore(img, obj);
+
+            $.ajax({
+                url: obj.href || obj.value,
+                type: 'get',
+                dataType: "json",
+                success: jQuery.nette.success.bind({url: obj.href || obj.value})
+            });
         }
     }
 });
 
+// TODO popstate ??
+// window.onpopstate = function(a) { window.location.reload(); };
 
 $("#main").on("click", "a.ajax", function (event) {
     if (!event.ctrlKey) {
         event.preventDefault();
-        $.ajax({
-            url: this.href,
-            type: 'get',
-            dataType: "json",
-            success: jQuery.nette.success
-        });
+        jQuery.nette.callUrl(this);
     }
 });
