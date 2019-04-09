1234b6b89221e1007bef870581db16e2774dafc4
Marián Kyral <mkyral@email.cz>
2019-04-09 05:45:40 +0200

    Use production Fody only on openstreetmap.cz and osmap.cz

--- js/osmcz-config.js
+++ js/osmcz-config.js
@@ -5,4 +5,4 @@ osmcz.production = ['openstreetmap.cz', 'osmap.cz', 'osm.localhost', 'devosm.zby
 osmcz.basePath = osmcz.production ? '/theme/' : '';
 osmcz.fakeHttps = osmcz.production ? '/proxy.php/' : 'http://';
 osmcz.user = false; //user object of currently logged in user. Defined later in @layout.latte
-osmcz.photoDbUrl = osmcz.production ? 'https://osm.fit.vutbr.cz/fody/' : 'https://osm.fit.vutbr.cz/fody-dev/';
+osmcz.photoDbUrl = (['openstreetmap.cz', 'osmap.cz'].indexOf(location.hostname) !== -1 ? 'https://osm.fit.vutbr.cz/fody/' : 'https://osm.fit.vutbr.cz/fody-dev/');
